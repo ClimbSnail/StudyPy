552abc043e120db8b65e95c1694c9552355fc6a6
heqi04 <heqi04@baidu.com>
2021-03-04 20:50:01 +0800

    add WebSocket

--- Flask/myflask.py
+++ Flask/myflask.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import Blueprint
 
 
 app = Flask(__name__)
 
 # 加入路由信息
+
+
 @app.route("/")
 def index():
     # 使用render_template，文件必须要同级目录的templates文件夹中
@@ -27,6 +30,22 @@ def login():
         # 使用render_template，文件必须要同级目录的templates文件夹中
         return render_template("login.html", msg="登录失败")
 
+
+# 蓝图设置
+url_prefix = "/v1/local/operations"
+show_local_blueprint = Blueprint(
+    'show_local',
+    __name__,
+    url_prefix=url_prefix
+)
+
+
+@show_local_blueprint.route("/hello")
+def list_tags():
+    return "world"
+
+
+app.register_blueprint(show_local_blueprint)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5001, debug=True)
---
+++ Flask/websocket.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request, render_template
+from flask_socketio import SocketIO, emit
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template('test.html')
+
+# 触发事件my event：回复只发送此连接
+
+
+@socketio.on('my_event', namespace='/chat')
+def test_message(message):
+    print(message)
+    emit('my_response', {'data': message['data']})
+
+
+# 触发事件my broadcast event：：回复所有链接（广播）
+@socketio.on('my_broadcast event', namespace='/chat')
+def test_message(message):
+    print(message)
+    emit('my_response', {'data': message['data']}, broadcast=True)
+
+##################################################################
+# 自动连接和自动断开触发
+
+
+@socketio.on('connect', namespace='/chat')
+def test_connect():
+    print("连接到来")
+    emit('my_response', {'data': 'Connected'})
+
+
+@socketio.on('disconnect', namespace='/chat')
+def test_disconnect():
+    print("连接断开")
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='127.0.0.1', port=8200, debug=True)
